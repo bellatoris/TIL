0edb9cbe52a27266ff9136c9530e4827b68c66db
bellatoris <bellatoris@snu.ac.kr>
2018-02-05 23:58:23 +0900

    fix build error

---
+++ Scala/FPinScala/practice/project/Dependencies.scala
@@ -0,0 +1,6 @@
+import sbt._
+
+
+object Dependencies {
+  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.4"
+}
---
+++ Scala/FPinScala/practice/src/main/scala/Chapter8/ListCheck.scala
@@ -0,0 +1,26 @@
+package Chapter8
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
+
+object Test {
+  val intList = Gen.listOf(Gen.choose(0, 100))
+  val prop =
+    forAll(intList)(ns => ns.reverse.reverse == ns) &&
+    forAll(intList)(ns => ns.headOption == ns.reverse.lastOption)
+
+  /**
+   * exercise 8.1
+   */
+  val sumProp = 
+    forAll(intList)(ns => ns.sum == ns.reverse.sum)
+
+  /**
+   * exercise 8.2
+   */
+  val maxProp = 
+    forAll(intList)(ns => ns.max == ns.reverse.max)
+
+  def main(args: Array[String]): Unit = {
+    println(prop.check)
+  }
+}
